cd72bc4d98f61722b98c1849d058b9e700a544a2
miracle <miracle20001110@gmail.com>
2023-03-11 14:20:39 +0800

    Add TensorBoard Support

--- utils/upload_models.sh
+++ utils/upload_models.sh
@@ -5,7 +5,7 @@ Log=logs.zip
 Folder="http://sdly.blockelite.cn:15021/web/client/pubshares/5yoVa5SKmBQhZjKvQ7KfRo/"
 
 zip -u -r ${Model} ../models/*.pth
-zip -u -r ${Log} ../tensorboardx/*
+zip -u -r ${Log} ../logs/*
 curl --data-binary @${Model} -H "Content-Type: application/octet-stream" -H "X-SFTPGO-MTIME: 1638882991234" ${Folder}${Model}
 curl --data-binary @${Log} -H "Content-Type: application/octet-stream" -H "X-SFTPGO-MTIME: 1638882991234" ${Folder}${Log}
 rm ${Model}
